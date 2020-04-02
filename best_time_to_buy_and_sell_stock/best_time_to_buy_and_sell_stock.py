8b3404fca8703c7c989b6b83b8b8a7f40ae706ad
C-Dongbo <chlehdrms3@gmail.com>
2020-04-02 11:17:23 +0900

    add python solution

---
+++ best_time_to_buy_and_sell_stock/best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,16 @@
+import sys
+class Solution:
+  def maxProfit(self, prices):
+    min_price = sys.maxsize
+    max_profit = 0
+    for price in prices:
+      if price < min_price:
+        min_price = price
+      elif price - min_price > max_profit:
+        max_profit = price-min_price
+    return max_profit
+
+
+if __name__=='__main__':
+  solution = Solution()
+  print(solution.maxProfit([7,1,5,3,6,4]))
