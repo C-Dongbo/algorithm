aa74676b1033e339294a66e127b0c4576d672585
C-Dongbo <chlehdrms3@gmail.com>
2020-07-07 09:26:37 +0900

    add recursive function

--- count_negative_number_in_a_sorted_matrix/solution.py
+++ count_negative_number_in_a_sorted_matrix/solution.py
@@ -16,9 +16,32 @@ class Solution:
                     low = mid + 1
         return cnt
 
+    def countNegatives2(self, grid):
+        global_cnt = 0
+
+        def rec(sorted_list, low, high, cnt):
+            
+            if low > high:
+                return cnt
+
+            mid = int((low+high)/2)
+
+            if sorted_list[mid] < 0:
+                print(sorted_list)
+                cnt += high - mid + 1
+                cnt = rec(sorted_list, low, mid-1, cnt)
+
+            else:
+                cnt = rec(sorted_list, mid+1, high, cnt)
+            return cnt
+        global_cnt = 0
+        for sorted_list in grid:
+            global_cnt = rec(sorted_list, 0, len(sorted_list)-1, global_cnt)
+
+        return global_cnt
 
 if __name__ =='__main__':
-    test = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
+    test = [[4,3,2,-1]]
     solution = Solution()
-    print(solution.countNegatives(test))
+    print(solution.countNegatives2(test))
         
